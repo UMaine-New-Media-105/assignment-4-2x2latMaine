3d9f303c74eba13a27e7626840a29bf601e6b215
2x2latMaine <124173816+2x2latMaine@users.noreply.github.com>
2023-04-03 10:44:21 -0400

    A pattern of randomly colored and oriented flowers

---
+++ maxxSmithA4Q3.js
@@ -0,0 +1,112 @@
+let flower1 = {
+  //Pink flower with 5 petals
+  startX: 115,
+  startY: 50,
+  size: 0.25,
+  petals: 5,
+  color: 300,
+  rotation: 30
+};
+
+let flower2 = {
+  //Blue flower with 7 petals
+  startX: 55,
+  startY: 100,
+  size: 0.2,
+  petals: 7,
+  color: 200,
+  rotation: 30
+};
+
+let flower3 = {
+  //White flower with 4 petals
+  startX: 30,
+  startY: 30,
+  size: 0.15,
+  petals: 4,
+  color: 60,
+  rotation: 30
+};
+
+let pattern = {
+  //Object to edit pattern spacing
+  rows: 4,
+  columns: 7,
+};
+
+function setup() {
+  createCanvas(960, 540);
+  angleMode(DEGREES);
+  colorMode(HSL);
+  noLoop();
+}
+
+function draw() {
+  background("midnightblue");
+
+  //Draws Columns & Rows
+  for (let columnsDrawn = 0; columnsDrawn < pattern.columns; columnsDrawn++) {
+    for (let rowsDrawn = 0; rowsDrawn < pattern.rows; rowsDrawn++) {
+      //Draw Columns
+      flower1.x = flower1.startX + (width / pattern.columns) * columnsDrawn;
+      flower2.x = flower2.startX + (width / pattern.columns) * columnsDrawn;
+      flower3.x = flower3.startX + (width / pattern.columns) * columnsDrawn;
+
+      //Draw Rows
+      flower1.y = flower1.startY + (height / pattern.rows) * rowsDrawn;
+      flower2.y = flower2.startY + (height / pattern.rows) * rowsDrawn;
+      flower3.y = flower3.startY + (height / pattern.rows) * rowsDrawn;
+
+      //Randomize Color
+      flower1.color = random(360);
+      flower2.color = random(360);
+      flower3.color = random(360);
+
+      //Randomize Rotation
+      flower1.rotation = random(360);
+      flower2.rotation = random(360);
+      flower3.rotation = random(360);
+
+      //Randomize Size
+      flower1.size = random(0.2, 0.3);
+      flower2.size = random(0.15, 0.25);
+      flower3.size = random(0.1, 0.2);
+
+      drawFlower(flower1);
+      drawFlower(flower2);
+      drawFlower(flower3);
+    }
+  }
+}
+
+function drawFlower(flower) {
+  push();
+  //Calls flower in location and size of choosing
+  translate(flower.x, flower.y);
+  scale(flower.size);
+
+  //Sets color to full saturation of degree called in color
+  fill(flower.color, 100, 50);
+
+  push();
+
+  rotate(flower.rotation);
+
+  noStroke();
+
+  //Petals
+  for (let i = 0; i < flower.petals; i++) {
+    rect(0, 0, 100, 100, 50);
+    rotate(360 / flower.petals); //Changes petal rotation based on # of petals
+  }
+  //Flower Bulb
+  if (flower.color >= 50 && flower.color <= 70) {
+    //If petals are yellow, bulb is brown
+    fill("sienna");
+  } else {
+    fill("yellow"); //All other colors, bulb is set to yellow
+  }
+  ellipse(0, 0, 100); //FLower bulb
+  pop();
+  pop();
+}
